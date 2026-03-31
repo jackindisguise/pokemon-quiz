b58544406bb5dfe22def6bf24fd5838a2e81d478
Curtis Erickson <incognitus.me@gmail.com>
2026-03-31 14:52:55 -0500

    chore: formatting

--- scripts/pokemon-move-scraper.js
+++ scripts/pokemon-move-scraper.js
@@ -9,39 +9,39 @@ import pokemonList from "../data/pokemon.json" with { type: "json" };
 import movesList from "../data/moves.json" with { type: "json" };
 
 function getMove(name) {
-	return movesList.find((move) => move.name === name);
+  return movesList.find((move) => move.name === name);
 }
 
 function getPokemonByName(name) {
-	return pokemonList.find((pokemon) => pokemon.pokemon === name);
+  return pokemonList.find((pokemon) => pokemon.pokemon === name);
 }
 
 // where to save moves
 const pokemonMovesPath = "data/replacement-pokemon-moves.json";
 
 function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 const replacement = [
-	"Nidoran♀",
-	"Nidoran♂",
-	"Farfetch'd",
-	"Mr. Mime",
-	"Deoxys",
-	"Wormadam",
-	"Mime Jr.",
-	"Kyurem",
-	"Flabébé",
-	"Meowstic",
-	"Hoopa",
-	"Lycanroc",
-	"Type: Null",
-	"Toxtricity",
-	"Sirfetch'd",
-	"Mr. Rime",
-	"Urshifu",
-	"Calyrex",
+  "Nidoran♀",
+  "Nidoran♂",
+  "Farfetch'd",
+  "Mr. Mime",
+  "Deoxys",
+  "Wormadam",
+  "Mime Jr.",
+  "Kyurem",
+  "Flabébé",
+  "Meowstic",
+  "Hoopa",
+  "Lycanroc",
+  "Type: Null",
+  "Toxtricity",
+  "Sirfetch'd",
+  "Mr. Rime",
+  "Urshifu",
+  "Calyrex",
 ];
 
 // prep
@@ -50,50 +50,50 @@ const browser = await launch();
 const page = await browser.newPage();
 const pokemonMoves = [];
 for (let i = 0; i < replacement.length; i++) {
-	// 3 tries
-	for (let j = 0; j < 3; j++) {
-		try {
-			const pokemon = getPokemonByName(replacement[i]);
-			const url = URL.replace(
-				"?",
-				(function () {
-					return pokemon.pokemon
-						.toLowerCase()
-						.replace(" ", "-")
-						.replace("♀", "-f")
-						.replace("♂", "-m")
-						.replace("'", "")
-						.replace(".", "")
-						.replaceAll("é", "e")
-						.replace(":", "");
-				})(),
-			).replace("9", `${9 - j}`);
-			console.log(url);
-			await page.goto(url);
-			let table = await page.$(
-				".sv-tabs-panel > div > div:nth-child(1) > div:nth-child(3) > table",
-			);
-			if (!table) table = await page.$(".sv-tabs-panel > div > table");
-			const rows = await table.$$("tbody > tr");
-			const moves = await Promise.all(
-				rows.map(async (row) => {
-					const cells = await row.$$("td");
-					const level = await cells[0].evaluate((el) =>
-						parseInt(el.textContent),
-					);
-					const moveName = await cells[1].evaluate((el) => el.textContent);
-					return { level, move: getMove(moveName).id };
-				}),
-			);
-			pokemonMoves.push({ pokemon: pokemon.id, moves });
-			break;
-		} catch (err) {
-			console.log(err);
-			console.log("Trying again...");
-		}
-		await sleep(1000);
-	}
-	await sleep(1000);
+  // 3 tries
+  for (let j = 0; j < 3; j++) {
+    try {
+      const pokemon = getPokemonByName(replacement[i]);
+      const url = URL.replace(
+        "?",
+        (function () {
+          return pokemon.pokemon
+            .toLowerCase()
+            .replace(" ", "-")
+            .replace("♀", "-f")
+            .replace("♂", "-m")
+            .replace("'", "")
+            .replace(".", "")
+            .replaceAll("é", "e")
+            .replace(":", "");
+        })(),
+      ).replace("9", `${9 - j}`);
+      console.log(url);
+      await page.goto(url);
+      let table = await page.$(
+        ".sv-tabs-panel > div > div:nth-child(1) > div:nth-child(3) > table",
+      );
+      if (!table) table = await page.$(".sv-tabs-panel > div > table");
+      const rows = await table.$$("tbody > tr");
+      const moves = await Promise.all(
+        rows.map(async (row) => {
+          const cells = await row.$$("td");
+          const level = await cells[0].evaluate((el) =>
+            parseInt(el.textContent),
+          );
+          const moveName = await cells[1].evaluate((el) => el.textContent);
+          return { level, move: getMove(moveName).id };
+        }),
+      );
+      pokemonMoves.push({ pokemon: pokemon.id, moves });
+      break;
+    } catch (err) {
+      console.log(err);
+      console.log("Trying again...");
+    }
+    await sleep(1000);
+  }
+  await sleep(1000);
 }
 await page.close();
 await browser.close();
--- scripts/pokemon2big.js
+++ scripts/pokemon2big.js
@@ -5,25 +5,25 @@ import pokemonMoves from "../data/pokemon-moves.json" with { type: "json" };
 import moves from "../data/moves.json" with { type: "json" };
 
 function getPokemonMovesById(id) {
-	return pokemonMoves.find((pokemon) => pokemon.pokemon === id);
+  return pokemonMoves.find((pokemon) => pokemon.pokemon === id);
 }
 
 const pokemon = pokemonList.map((pokemon) => {
-	const myMoves = getPokemonMovesById(pokemon.id);
-	if (!myMoves) {
-		console.log(pokemon.pokemon);
-		return pokemon;
-	}
-	const filtered = [];
-	for (let move of myMoves.moves) {
-		filtered.push({ id: move.move, level: move.level });
-	}
-	return {
-		id: pokemon.id,
-		pokemon: pokemon.pokemon,
-		generation: pokemon.generation,
-		type: pokemon.type,
-		moves: filtered,
-	};
+  const myMoves = getPokemonMovesById(pokemon.id);
+  if (!myMoves) {
+    console.log(pokemon.pokemon);
+    return pokemon;
+  }
+  const filtered = [];
+  for (let move of myMoves.moves) {
+    filtered.push({ id: move.move, level: move.level });
+  }
+  return {
+    id: pokemon.id,
+    pokemon: pokemon.pokemon,
+    generation: pokemon.generation,
+    type: pokemon.type,
+    moves: filtered,
+  };
 });
 await writeFile("data/pokemon-big.json", JSON.stringify(pokemon, null, 2));
